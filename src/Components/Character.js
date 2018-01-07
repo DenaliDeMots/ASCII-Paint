462fa6f998304facbbc51174567ac4e4bca2329b
DenaliDeMots <denali.demots@gmail.com>
2018-01-07 17:40:30 -0500

    fixed error using react keys

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -3,8 +3,8 @@ import React from 'react'
 //This component represents a single ASCII character
 //It is effectively like a pixel on the canvas
 
-const Character = ({character, key, style, onMouseMove}) => (
-    <div key={key} style={style} onMouseMove={onMouseMove}>
+const Character = ({character, style, onMouseMove}) => (
+    <div style={style} onMouseMove={onMouseMove}>
         {character}
     </div>
 )
